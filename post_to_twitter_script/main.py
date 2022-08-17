47c6d944b5b3279a5753f1f650d80f2a22e9ef5b
Johans Justs Eris <johans.eris@r6vsk.lv>
2022-08-17 19:46:39 +0300

    Added password_gen.py

---
+++ post_to_twitter_script/main.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append(".")
+# print(password_gen.generate_password())
+# from generate_password_script.password_gen import *
+# from generate_password_script import *
+import generate_password_script.password_gen as passgen
+
+
+print(passgen.generate_password())
+
