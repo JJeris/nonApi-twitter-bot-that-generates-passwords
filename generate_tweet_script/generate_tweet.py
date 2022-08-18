e37dacc7ce711c8980a38f27b6ac95c56c790578
Johans Justs Eris <johans.eris@r6vsk.lv>
2022-08-18 13:26:21 +0300

    Added changes to functions

---
+++ generate_tweet_script/generate_tweet.py
@@ -0,0 +1,3 @@
+def generate_tweet(password):
+    string = f"The password of the day is: {password}"
+    return string    
--- post_to_twitter_script/main.py
+++ post_to_twitter_script/main.py
@@ -3,25 +3,38 @@ import sys
 sys.path.append(".")
 # print(password_gen.generate_password())
 # from generate_password_script.password_gen import *
-from generate_password_script import *
+# from generate_password_script import *
 import generate_password_script.password_generator as passgen
+import generate_tweet_script.generate_tweet as tweetgen
 
+
+
+### Functions section
+def retrieve_username_password():
+    """
+    The path to this .txt file should be defined by the user himself. It cotains the twitter username (1st line) and the accounts password (2nd) line.
+    
+    The username and password are stored in a txt file which is outisde of the projects scope, so as to ensure that the login info cannot be viewed on github.
+    """
+    f = open(r'C:\Users\usr\Desktop\OFFICE\Macibas_projekti\Web\password_twitter_bot_generate_password.txt', 'r')
+    login_info = f.read().split("\n")
+    f.close()
+    return login_info
 
 
 
 ### Integration section
-print(passgen.generate_password(10))
-# f = open("D:\\myfiles\welcome.txt", "r")
-# print(f.read())
-
-print(f.read())
 
 
-
-
-### DEMO SECTION
-# try:
-#   print(passgen.generate_password(0))
-# finally: # I don't know why this works, but it does, so... Might need to rework the raising of the error msg in password_generator.py
-#     print("Try block finished.")
+### Demo section
+try:
+    print()
+    login_arr = retrieve_username_password()
+    print(login_arr)
+    password = passgen.generate_password()
+    print(password)
+    tweet = tweetgen.generate_tweet(password)
+    print(tweet)
+finally:
+    print("\n#######Try/Catch block has finished succesfully#######")
   
